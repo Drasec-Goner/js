6d9b91dac760e504368a3a2622e8db69969dd0cb
Drasec Goner <drasec.04@gmail.com>
2024-02-27 10:53:06 +0530

    Practice

---
+++ express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/nig', (req,res) => {
+    res.send('Hello, Nigga!');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
